44d52621184a762f332027794056614e8edcd31c
davidcp <pereiracostadavid@gmail.com>
2023-04-09 08:48:16 -0300

    Initial Commit

--- IC_GPS.py
+++ IC_GPS.py
@@ -1,83 +1,10 @@
-import datetime as dt
-import os
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
-import seaborn as sns
-
+import F_GPS
 '''----------------------------------------------------------------------'''
-'''Derivada númerica'''
-
-
-def Deriv_Ord1(xi, yi):
-    n = len(xi)
-    deriv = np.zeros(n)
-    '''Para o primeiro ponto'''
-    deriv[0] = (yi[1] - yi[0]) / (xi[1] - xi[2])
-    '''Para os demais pontos'''
-    for k in range(1, n - 1):
-        deriv[k] = (yi[k + 1] - yi[k - 1]) / (xi[k + 1] - xi[k - 1])
-    '''Para o último ponto'''
-    deriv[n - 1] = (yi[n - 1] - yi[n - 2]) / (xi[n - 1] - xi[n - 2])
-    return deriv
-
-
-'''----------------------------------------------------------------------'''
-
+# arquivo in (x for x in arquivos if 'Cmn' in x)
 '''Ler todos os arquivos da pasta'''
-arquivos = os.listdir('Input_GPS')
-'''Fazer o tratamento de dados para cada arquivo'''
-for arquivo in arquivos:
-    ler = open(f'Input_GPS/{arquivo}', 'r')
-    for i in range(2):
-        ler.readline()
-    c = ler.readline()
-    c = c.split()
-    for i in range(2):
-        ler.readline()
-    coordenadas = [c[0], c[1], c[2]]
-    n_stations = []
-    Dados = [[[] for _ in range(3)] for _ in
-             range(33)]  # numero do satelite(32 satelites), tempo e Vtec para cada satelite
-    elev = int(15)  # int(input('Elevacao minima:'))
-    for linha in ler:
-        if linha:
-            linha = linha.split()
-            '''Tempo negativo = ausência de dados. Elevação mínima do satélite'''
-            if float(linha[1]) >= 0 and float(linha[4]) >= elev and linha[1] and linha[8] and linha[5]:
-                Dados[int(linha[2])][0].append(float(linha[1]))
-                Dados[int(linha[2])][1].append(float(linha[8]))
-                Dados[int(linha[2])][2].append(float(linha[5]))
-
-    plt.figure(dpi=500)
-    '''Latitudes máxima e mínima'''
-    max_color = []
-    min_color = []
-    for i in range(1, 33):
-        if Dados[i][2]:
-            max_color.append(max(Dados[i][2]))
-            min_color.append(min(Dados[i][2]))
-    x1 = max(max_color)
-    x2 = min(min_color)
-    '''--------------------------------------'''
-    '''Plotar 'Vtec vs tempo' para todos os satélites'''
-    for i in range(1, 33):
-        if Dados[i][2]:
-            plt.scatter(Dados[i][0], Dados[i][1], s=0.1, marker=',', c=Dados[i][2], vmin=x2, vmax=x1, cmap='gist_ncar')
-    plt.xlabel('UT (hrs)')
-    plt.ylabel('TEC units')
-    plt.colorbar(label='Latitude')
-    Date = str(dt.datetime.today())
-    plt.text(0, 54, f'Date:{Date[:10]} (Elev Mask {elev} deg)')
-    plt.text(0, 56, f'Latitude(deg):{c[0]} Longitude(deg):{c[1]} Altitude(m):{c[2]}')
-    plt.show()
-    '''---------------------------------------------------------------------------'''
-    '''Plotar o ROTi (para cada satélite)'''
-    for i in range(1, 33):
-        x = np.array(Dados[i][0])
-        y = np.array(Dados[i][1])
-        if len(x) == len(y) and len(x) > 0:
-            rot = Deriv_Ord1(x, y)
-            plt.plot(x, rot)
-    plt.show()
+F_GPS.Vtec_vs_t(['braz'], 14, 1)
+# Brasilia, Belém, Uruaçu, Goiânia, Florianópolis, Porto Alegre, Lages, Marabá
+F_GPS.ROT(['bele',  'gour', 'braz', 'gogy', 'ifsc', 'poal'], [13, 14, 15, 16, 17, 18], 1)
+F_GPS.ROTi('braz', 14, 1)
+dias_calmos = [5, 6, 7, 10, 11, 12, 13, 24]
+F_GPS.Tec_vs_tec_medio(['bele', 'maba', 'gour', 'braz', 'gogy', 'ifsc', 'poal'], 13, 18, dias_calmos, 1)
--- line_charts.py
+++ line_charts.py
@@ -1,15 +1,3 @@
-import pandas as pd
-pd.plotting.register_matplotlib_converters()
-import matplotlib.pyplot as plt
-import seaborn as sns
-import os
-
-#Read the data
-spotify_filepath = 'input/spotify.csv'
-spoify_data = pd.read_csv(spotify_filepath, index_col='Date', parse_dates=True)
-#Plot the data
-plt.figure(figsize=(14, 6))
-plt.title("Daily Global Streams of Popular Songs in 2017-2018")
-sns.lineplot(data=spoify_data['Despacito'], label='Despacito')
-sns.lineplot(data=spoify_data['Shape of You'], label='Shape of You')
-plt.show()
+a =32
+a = ('%03d' % a)
+print(a)
